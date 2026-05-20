d139eabb9a130fb909e3f66aabee92f5c0f3c377
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-05-20 22:15:38 +0800

    refactor: mengubah struktur db.js menyesuaikan dengan Database Prisma

--- src/config/db.js
+++ src/config/db.js
@@ -1,33 +1,42 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool } from 'pg'
+import pg from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const pool = new Pool(
-    { 
-        connectionString: process.env.DATABASE_URL  
-    }
-)
+const { Pool } = pg;
 
-const adapter = new PrismaPg(pool)
-
-const prisma = new PrismaClient({ 
-        adapter,
-        log: process.env.DATABASE_URL === "development" ? ["query", "error", "warn"] : ["error"]
-    }
-);
+let pool;
+let adapter;
+let prisma;
 
 const connectDB = async () => {
     try {
-        await prisma.$connect()
-        console.log("DB Connected via Prisma")
+        if (!prisma) {
+            pool = new Pool({ 
+                connectionString: process.env.DATABASE_URL  
+            });
+            adapter = new PrismaPg(pool);
+            prisma = new PrismaClient({ 
+                adapter,
+                log: process.env.DATABASE_URL === "development" ? ["query", "error", "warn"] : ["error"]
+            });
+        }
+
+        await prisma.$connect();
+        console.log("DB Connected via Prisma");
     } catch (error) {
         console.error("Database Connection Error:", error);
-        process.exit(1); // Exit the process with an error code
+        process.exit(1); 
     }
 };
 
 const disconnectDB = async () => {
-        await prisma.$disconnect()
-}
+    if (prisma) {
+        await prisma.$disconnect();
+    }
+    if (pool) {
+        await pool.end();
+    }
+};
 
-export { pool, adapter, prisma, connectDB, disconnectDB };
+export { connectDB, disconnectDB };
+export { prisma };
