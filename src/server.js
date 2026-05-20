529e71e9cbef385fc7966bd3517cdc75063645ba
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-05-20 16:25:32 +0800

    feat: menambahkan Routes dan penggunaan API routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
-const express = require('express');
+import express from "express";
+
+
+// Import Routes
+import movieRoutes from './routes/movieRoutes.js';
 
 const app = express();
 
+// API Routes
+app.use('/api', movieRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
 
 const PORT = 3000;
-
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
+    console.log(`Server is running on PORT http://localhost:${PORT}`);
 });
